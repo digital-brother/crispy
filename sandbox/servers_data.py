c9198ecdc21e49d75b5ce4d7ee3b1ab08e04f0fa
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2024-03-13 13:34:55 +0200

    Switch from queryset display to list of dicts. Table Meta model and fields => Table custom columns with SingleTableView.get_table_data()

---
+++ sandbox/servers_data.py
@@ -0,0 +1,27 @@
+SERVERS_DATA = [
+    {
+        "domain": "google.com",
+        "city": "New York",
+        "ip": "8.8.8.8"
+    },
+    {
+        "domain": "facebook.com",
+        "city": "San Francisco",
+        "ip": "1.2.3.4"
+    },
+    {
+        "domain": "twitter.com",
+        "city": "London",
+        "ip": "12.343.12.49"
+    },
+    {
+        "domain": "bing",
+        "city": "Stokholm",
+        "ip": "1.59.574.32"
+    },
+    {
+        "domain": "mozilla",
+        "city": "Zurich",
+        "ip": "3.76.244.32"
+    }
+]
